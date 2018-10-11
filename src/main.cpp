21816504cb7a3f6ed29a730a2b005a7f0808ac68
jazminbelmonte <jazminbelmonte@gmail.com>
2018-10-11 16:39:38 -0600

    added testing in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int main(){
 
-  return 0;
+    //PALINDROME
+    cout << palindrome("A car, a man, a maraca.") << endl;
+    cout << palindrome("A Toyota! Ra;;;;ce fast, safe car! A Toyota!!!!!!") << endl;
+
+    //BALANCEDBRACKETS
+    cout << balancedBrackets("(<[]>)") << endl;
+    cout << balancedBrackets("(this is()a test[[]])") << endl;
+
+    //POSTFIX
+    cout << (evaluatePostfixExpression("4 2 + 3 5 1 -  * + ")== -6) << endl;
+    cout << (evaluatePostfixExpression("10 2 8 * + 3 -") == -23) << endl;
+
+    return 0;
 }
